9fa58736566c7ba9be880b86bbbcedf232f71e2f
mbIM <lina.alina.fedotova@gmail.com>
2019-11-19 11:19:14 +0300

    b

---
+++ geniy/iluxa/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace iluxa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int a = rnd.Next(69, 1265);
+
+            Console.WriteLine(a);
+        }
+    }
+}
---
+++ gg1/gg1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace gg1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int b = 1;
+            int temp = 0;
+            int c = 100;
+            int a = rnd.Next(b, c);
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            do
+            {
+                if (num > a)
+                {
+                    temp = a;
+                    a = rnd.Next(temp, c);
+                    Console.WriteLine(a);
+                }
+                else if (num < a)
+                {
+                    temp = a;
+                    a = rnd.Next(b, temp);
+                    Console.WriteLine(a);
+                }
+                else if (num == a)
+                {
+                    Console.WriteLine(a);
+                }
+            } while (num != a);
+        }
+    }
+}
